3a94351c702c2f96b5420afff59b9dfe5df87ffd
frieser <fiti.pol@gmail.com>
2012-02-16 21:04:00 +0100

    creados varios overlays sobre el mapa

--- src/Agoratec/VueloAppBundle/Resources/public/js/home.js
+++ src/Agoratec/VueloAppBundle/Resources/public/js/home.js
@@ -1,4 +1,6 @@
 var map;
+var marker;
+var infowindow;
 var caceres = new google.maps.LatLng(39.481785, -6.369324);
 
 $(document).ready(function(){
@@ -29,6 +31,50 @@ $(document).ready(function(){
     map = new google.maps.Map(document.getElementById("map_canvas"),
         myOptions);
 
+ marker = new google.maps.Marker({
+      position: caceres,
+      title:"Su posicion!"
+  });
+
+	var flightPlanCoordinates = [
+    new google.maps.LatLng(38.899583, -6.984558),
+    new google.maps.LatLng(39.476486, -6.391296),
+    new google.maps.LatLng(39.991851, -4.817505),
+    new google.maps.LatLng(40.027614, -6.083679)
+  ];
+  var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+	map: map,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.5,
+    strokeWeight: 10
+  }); 
+
+	var triangleCoords = [
+    new google.maps.LatLng(39.713525, -6.534119),
+    new google.maps.LatLng(39.726201, -6.880188),
+    new google.maps.LatLng(39.234381, -7.003784),
+    new google.maps.LatLng(39.438314, -6.583557)
+  ];
+
+bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
+    strokeColor: "#000000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#0000FF",
+    fillOpacity: 0.35
+  });
+
+infowindow = new google.maps.InfoWindow({
+    content: "Usted esta aqui!"
+});
+
+google.maps.event.addListener(marker, 'click', function() {
+  infowindow.open(map,marker);
+});
+
+ bermudaTriangle.setMap(map);
 	var homeControlDiv = document.createElement('DIV');
   var homeControl = new HomeControl(homeControlDiv, map);
 
@@ -64,7 +110,9 @@ function HomeControl(controlDiv, map) {
 
   // Setup the click event listeners: simply set the map to Chicago
   google.maps.event.addDomListener(controlUI, 'click', function() {
-    map.setCenter(caceres)
+    map.setCenter(caceres);
+	marker.setMap(map);
+	infowindow.open(map,marker);
   });
 }
 
